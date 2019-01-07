2e5abdf50e8085604184f7204ab3fb1730d72b27
Max Lv <max.c.lv@gmail.com>
2019-01-07 17:02:10 +0800

    Set default autoexpire to 10

--- client/main.go
+++ client/main.go
@@ -124,7 +124,7 @@ func main() {
 		},
 		cli.IntFlag{
 			Name:  "autoexpire",
-			Value: 0,
+			Value: 10,
 			Usage: "set auto expiration time(in seconds) for a single UDP connection, 0 to disable",
 		},
 		cli.IntFlag{
