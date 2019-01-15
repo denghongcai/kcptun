562394919c51aab06f6ac3a260eb300ffcc7147f
Mygod <contact-github@mygod.be>
2019-01-15 18:49:29 +0800

    Fix some bugs

--- client/utils.go
+++ client/utils.go
@@ -7,3 +7,6 @@ import kcp "github.com/xtaci/kcp-go/v5"
 func DialKCP(raddr string, block kcp.BlockCrypt, dataShards, parityShards int) (*kcp.UDPSession, error) {
 	return kcp.DialWithOptions(raddr, block, dataShards, parityShards)
 }
+
+func log_init() {
+}
